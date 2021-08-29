a5c6deabc1396648399a658a6b1ac056c711f895
Sidney Santos <sidney_ssantos@hotmail.com>
2021-08-29 17:20:02 -0300

    04.01.12 O que aprendemos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  title = 'a11y-p1';
+  title = 'Acessibilidade';
   public yesNoAnswer = 'no';
 }
--- src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
+++ src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -1,18 +1,34 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-yes-no-button-group',
   templateUrl: './yes-no-button-group.component.html',
   styleUrls: ['./yes-no-button-group.component.scss']
 })
+
 export class YesNoButtonGroupComponent implements OnInit {
 
   @Input() public value: string = null;
   @Input() public label = '';
+
+  @Output() public valueChange = new EventEmitter<string>();
+
+  public options = YesNoButtonGroupOptions;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public activate(value: string): void {
+    this.value = value;
+
+    // notificando o mundo externo que o valor mudou
+    this.valueChange.emit(this.value);
+  }
+}
+
+enum YesNoButtonGroupOptions {
+  YES = 'yes',
+  NO = 'no'
 }
